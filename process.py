51b76af0a2c1e5c227835652c30306bae6bf855b
Parthav Joshi <parthavjoshi1718@gmail.com>
2022-04-28 16:47:43 +0530

    Init

--- process.py
+++ process.py
@@ -16,40 +16,37 @@ y = df.Outcome
 X_train, X_test, y_train, y_test = train_test_split(X,y,random_state=42, stratify=y)
 
 
-for depth in range(1, 25):
-    scaler = preprocessing.StandardScaler()
-    x_train = scaler.fit_transform(X_train)
-    x_valid = scaler.transform(X_test)
+scaler = preprocessing.StandardScaler()
+x_train = scaler.fit_transform(X_train)
+x_valid = scaler.transform(X_test)
 
-    cat = LGBMClassifier(
-        max_depth=depth,
-        n_estimators=700,
-        random_state=42,
-        learning_rate=0.01,
-        verbose=0,
-        reg_alpha=25,
-        early_stopping_rounds=1000
-    )
+cat = LGBMClassifier(
+    max_depth=3,
+    n_estimators=700,
+    random_state=42,
+    learning_rate=0.01,
+    verbose=0,
+    reg_alpha=25,
+    early_stopping_rounds=1000
+)
 
-    cat.fit(x_train, y_train, eval_set=[(x_valid, y_test)])
-    preds_train = cat.predict(x_train)
-    preds_valid = cat.predict(x_valid)
+cat.fit(x_train, y_train, eval_set=[(x_valid, y_test)])
+preds_train = cat.predict(x_train)
+preds_valid = cat.predict(x_valid)
 
-    train_accuracy = metrics.accuracy_score(y_train, preds_train)
-    test_accuracy = metrics.accuracy_score(y_test, preds_valid)
+train_accuracy = metrics.accuracy_score(y_train, preds_train)
+test_accuracy = metrics.accuracy_score(y_test, preds_valid)
 
-    train_acc.append(train_accuracy)
-    pred_acc.append(test_accuracy)
+train_acc.append(train_accuracy)
+pred_acc.append(test_accuracy)
 
-plt.plot(train_acc, label="train")
-plt.plot(pred_acc, label="test")
-plt.xlabel('max_depth', size = 20)
-plt.ylabel('accuracy', size=20)
-plt.legend(loc = 'upper left', prop = {'size':15})
-plt.show()
+# plt.plot(train_acc, label="train")
+# plt.plot(pred_acc, label="test")
+# plt.xlabel('max_depth', size = 20)
+# plt.ylabel('accuracy', size=20)
+# plt.legend(loc = 'upper left', prop = {'size':15})
+# plt.show()
 
-# print(model[1].predict([[4,120,34,43,72,12,0.77,0]]))
-# print(train_accuracy)
 print(pred_acc)
 # create an iterator object with write permission - model.pkl
 with open(f'model.pkl', 'wb') as files:
